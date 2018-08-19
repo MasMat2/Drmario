a31912e4394da4d7e42e23594e410d24d8630f8f
Max <maaxx.360@gmail.com>
2018-08-19 01:00:37 -0500

    Mario block physics

---
+++ pseudomario.py
@@ -0,0 +1,20 @@
+
+create pygame interface
+create player object
+create block objects
+
+
+
+
+
+check if valid move
+    if tail change valid
+        add change tail to queue
+    if move valid
+        add move to queue
+
+for events in queue
+    execute events
+
+render blocks
+render player
